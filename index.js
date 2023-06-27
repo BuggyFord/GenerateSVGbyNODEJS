d0a4acbd61dcdcff4bd80be96257179fd8b8734f
BuggyFord <128647975+BuggyFord@users.noreply.github.com>
2023-06-27 17:40:01 -0500

    added code for test file and adjusted other stuff.

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const 
-
-
-
-
-
-
+const { Circle, Triangle, Square} = require('./shapes');
+const {writeFile} = require('fs/promises');
 
 
 function promptQuestions(){
@@ -16,28 +10,49 @@ function promptQuestions(){
         type: 'input',
         name: 'text',
         message:'Enter text for the logo (It must not be more than three characters.)',
+        validate: (text) =>
+        text.length <= 3 ||
+        "The logo must not be more than three characters",
     },
     {
         type: 'input',
-        name: 'text color',
+        name: 'textColor',
         message:'Enter a color for the text',
     },
     {
         type: 'input',
-        name: 'shape',
+        name: 'shapeType',
         message: 'Select a color of the shape',
         choices: ['Square', 'Circle', 'Triangle']
     },
     {
         type: 'input',
-        name: 'shape color',
+        name: 'shapeColor',
         message: 'What color would you like the shape to be?'
     }
 
 ])
-    
+.then(({text, textColor, shapeType, shapeColor}) => {
+    let shape =
+    switch(shapeType) {
+        case 'Square':
+            let newShape = new Square();
+            break;
+        case 'Circle':
+            let newCircle = new Circle()
+            break;
+        case 'Triangle':
+            let newTriangle = new Triangle();
+            break;
+    }
+
+    let newSvg = new SVG();
+    newSvg.set
+})  
+}
+
     
 
-}
+
 
 promptQuestions();
