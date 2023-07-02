50e0c62031242ca26151b4695395c7883c0826b5
BuggyFord <128647975+BuggyFord@users.noreply.github.com>
2023-07-02 17:25:32 -0500

    refactored code and got the images to generate

---
+++ tests/shapes.test.js
@@ -0,0 +1,7 @@
+const { Triangle, Square, Circle } = require('../lib/shapes');
+
+test('Make sure that the info entered when the node js command is entered into prompt comes out correct', () => {
+    expext(
+        Square
+    )
+})
